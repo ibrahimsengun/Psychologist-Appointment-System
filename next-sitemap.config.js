7f18d20c9b2a9ceb195eab34cb68ae1b0d55045a
Ibrahim Sengun <ibrahimsengun173@gmail.com>
2025-08-16 13:26:54 +0300

    Add next-sitemap configuration and update package dependencies
    - Created a new configuration file for next-sitemap to automate sitemap generation and robots.txt creation
    - Added next-sitemap as a dependency in package.json and updated package-lock.json accordingly
    - Included a postbuild script to run next-sitemap after the build process

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://lokmanyilmaz.vercel.app', // kendi domainini yaz
+  generateRobotsTxt: true, // robots.txt de otomatik oluşsun
+  sitemapSize: 5000
+};
